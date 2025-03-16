c9ece645fb4510c558e20b2dd6f4864cf3317e8a
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-03-16 16:54:06 +0530

    3174. Clear Digits.java

---
+++ 3174. Clear Digits.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public String clearDigits(String s) 
+    {
+        Stack<Character>stack=new Stack<>();
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(!Character.isDigit(s.charAt(i)))
+            {
+                stack.push(s.charAt(i));
+            }
+            else
+            {
+                if(!stack.isEmpty())
+                {
+                    stack.pop();
+                }
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!stack.isEmpty())
+        {
+            sb.insert(0,stack.pop());
+        }
+        return sb.toString();
+    }
+}
