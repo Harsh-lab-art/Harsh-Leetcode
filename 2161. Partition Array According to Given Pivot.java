a04a7232b06356547a279a26a52a9301be9270bd
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-03-25 22:14:10 +0530

    2161. Partition Array According to Given Pivot.java

---
+++ 2161. Partition Array According to Given Pivot.java
@@ -0,0 +1,45 @@
+class Solution 
+{
+    public int[] pivotArray(int[] nums, int pivot) 
+    {
+        int lc=0;
+        int gc=0;
+        int pc=0;
+        for(int num:nums)
+        {
+            if(num<pivot)
+            {
+                lc++;
+            }
+            else if(num>pivot)
+            {
+                gc++;
+            }
+            else{
+                pc++;
+            }
+        }
+        int i=0;
+        int j=lc;
+        int k=lc+pc;
+        int []res=new int[nums.length];
+        for(int num:nums)
+        {
+            if(num<pivot)
+            {
+                res[i]=num;
+                i++;
+            }
+            else if(num>pivot)
+            {
+                res[k]=num;
+                k++;
+            }
+            else{
+                res[j]=num;
+                j++;
+            }
+        }
+        return res;
+    }
+}
