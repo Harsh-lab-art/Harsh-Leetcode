84a31162bcaef201317c304f28cc8615edd8a175
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-04-17 20:32:19 +0530

    2309. Greatest English Letter in Upper and Lower Case.java

---
+++ 2309. Greatest English Letter in Upper and Lower Case.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public String greatestLetter(String s) 
+    {
+        for(char ch='Z';ch>='A';ch--)
+        {
+            if(s.indexOf(ch)!=-1 && s.indexOf(Character.toLowerCase(ch))!=-1)
+            {
+                return String.valueOf(ch);
+            }
+        }
+        return "";   
+    }
+}
