11098d0e7cb484141c3604f5e50b07459100995e
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-04-18 23:56:24 +0530

    2037. Minimum Number of Moves to Seat Everyone.java

---
+++ 2037. Minimum Number of Moves to Seat Everyone.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution 
+{
+    public int minMovesToSeat(int[] seats, int[] students) 
+    {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int mov=0;
+        for(int i=0;i<seats.length;i++)
+        {
+            mov+=Math.abs(seats[i]-students[i]);
+        }
+        return mov;
+    }
+}
