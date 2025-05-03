bfa8f52094d537f0a92d1a68ace44206dde3cb7c
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-05-03 14:29:53 +0530

    2273. Find Resultant Array After Removing Anagrams.java

---
+++ 2273. Find Resultant Array After Removing Anagrams.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> result = new ArrayList<>();
+        String prevSorted = "";
+
+        for (String word : words) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String currSorted = new String(chars);
+
+            if (!currSorted.equals(prevSorted)) {
+                result.add(word);
+                prevSorted = currSorted;
+            }
+        }
+
+        return result;
+    }
+}
