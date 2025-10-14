6a05ebf799af82413667318447f163fd315a37ff
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-10-14 15:56:18 +0530

    3349. Adjacent Increasing Subarrays Detection I.java

---
+++ 3349. Adjacent Increasing Subarrays Detection I.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) 
+    {
+        int n = nums.size();
+        for (int i = 0; i + 2 * k <= n; i++) 
+        {
+            boolean firstIncreasing = true;
+            boolean secondIncreasing = true;
+            for (int j = i; j < i + k - 1; j++) 
+            {
+                if (nums.get(j) >= nums.get(j + 1)) 
+                {
+                    firstIncreasing = false;
+                    break;
+                }
+            }
+            for (int j = i + k; j < i + 2 * k - 1; j++) 
+            {
+                if (nums.get(j) >= nums.get(j + 1)) 
+                {
+                    secondIncreasing = false;
+                    break;
+                }
+            }
+            if (firstIncreasing && secondIncreasing) 
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
