0fdb3ee964102e78d8caafda3f45fb1c0ca1f9f1
Harsh Kumar Verma <harsh9760verma@gmail.com>
2026-02-08 20:19:11 +0530

    Implement balanced binary tree checker

---
+++ 110. Balanced Binary Tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    public boolean isBalanced(TreeNode root) 
+    {
+        return getheight(root)!=-1;
+    }
+    private int getheight(TreeNode node)
+    {
+        if(node ==null)
+        {
+            return 0;
+        }
+        int lefth=getheight(node.left);
+        if(lefth==-1)
+        return -1;
+        int righth=getheight(node.right);
+        if(righth==-1)
+        return -1;
+        if(Math.abs(lefth-righth)>1)
+        {
+            return -1;
+        }
+        return Math.max(lefth,righth)+1;
+    }
+}
