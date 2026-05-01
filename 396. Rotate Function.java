d55ebabac01feabc273495699377f7456f6aaf08
Harsh Kumar Verma <harsh9760verma@gmail.com>
2026-05-01 14:08:03 +0530

    Add maxRotateFunction to calculate rotation sums
    Implement maxRotateFunction to calculate the maximum rotation sum for an array.

---
+++ 396. Rotate Function.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int maxRotateFunction(int[] nums) 
+    {
+        int f=0;
+        int n=nums.length;
+        int nS=Arrays.stream(nums).sum();
+        for(int i=0;i<n;i++)
+        {
+            f+=i*nums[i];
+        }
+        int res=f;
+        for(int i=n-1;i>0;i--)
+        {
+            f+=nS-n*nums[i];
+            res=Math.max(res,f);
+        }
+        return res;
+    }
+}
